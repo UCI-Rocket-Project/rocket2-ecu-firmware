466786698f06777201b5e51ab46aa779cd638c3f
vqxel <rezq.bagheri@gmail.com>
2025-11-03 12:01:32 -0800

    Updated LoRA sync word

--- src/main.cc
+++ src/main.cc
@@ -184,7 +184,7 @@ MagBmi150i2c magnetometer(&hi2c1, MAG_INT_GPIO_Port, MAG_INT_Pin, MAG_DRDY_GPIO_
 
 // MAJOR NOTE, BAUD RATE PRESCALAR HAS TO BE SPI_BAUDRATEPRESCALER_8
 RadioSx127xSpi radio(&hspi5, RADIO_nCS_GPIO_Port, RADIO_nCS_Pin, RADIO_nRST_GPIO_Port, 
-                      RADIO_nRST_Pin, 0x12, RadioSx127xSpi::RfPort::PA_BOOST, 915000000, 15, 
+                      RADIO_nRST_Pin, 0x12, RadioSx127xSpi::RfPort::PA_BOOST, 915000000, 17, 
                       RadioSx127xSpi::RampTime::RT40US, RadioSx127xSpi::Bandwidth::BW125KHZ, 
                       RadioSx127xSpi::CodingRate::CR45, RadioSx127xSpi::SpreadingFactor::SF7, 
                       8, true, 10023, 10023);
